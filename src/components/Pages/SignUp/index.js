60aef71d51be33ead336a0e427b77a51b7aaab8a
Michael Haslam <ongomobile@gmail.com>
2021-06-20 14:43:34 -0700

    Fix build errors

--- src/components/Pages/SignUp/index.js
+++ src/components/Pages/SignUp/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { compose } from 'recompose';
 import InputFloatLabel from '../../UI_Components/Input';
