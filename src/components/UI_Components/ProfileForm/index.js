1cbd7ee2d980904bc6c9212fe40779012f893fde
Michael Haslam <ongomobile@gmail.com>
2021-06-20 18:01:41 -0700

    Build issue

--- src/components/UI_Components/ProfileForm/index.js
+++ src/components/UI_Components/ProfileForm/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { withFirebase } from '../../Firebase';
 import * as ROUTES from '../../../constants/routes';
